05b30e3086e453e14de3671f2d32839c322222a9
Oleksandr Bulakaiev <bulakaev@gmail.com>
2018-04-06 00:20:37 +0300

    check iterations count

--- Week1/AnalysisOfAlgorithms/ThreeSum.java
+++ Week1/AnalysisOfAlgorithms/ThreeSum.java
@@ -8,31 +8,35 @@ public class ThreeSum {
     public ThreeSum(int[] data) {
         this.data = data;
         Arrays.sort(this.data);
+        StdOut.println(data.length);
     }
 
     public void Find(int target) {
+        int iteration = 0;
         for (int i = 0; i < data.length - 2; i++) {
-            int partial_target = target - data[i];
+            iteration++;
             int j = i+1;
             int k = data.length - 1;
             while (j < k) {
-                int partial_sum = data[j] + data[k];
-                if (partial_sum == partial_target) {
+                iteration++;
+                int sum = data[i] + data[j] + data[k];
+                if (sum == target) {
                     StdOut.printf("%d %d %d", data[j], data[k], data[i]);
                     StdOut.println();
                     k--;
                     j++;
-                } else if (partial_sum > partial_target) {
+                } else if (sum > target) {
                     k--;
-                } else if (partial_sum < partial_target) {
+                } else if (sum < target) {
                     j++;
                 }
             }
         }
+        StdOut.println(iteration++);
     }
 
     public static void main(String[] args) {
-        ThreeSum threeSum = new ThreeSum(new int[]{-2, 4, 8, 1, 23, 12, 5, 0, 9, -3});
+        ThreeSum threeSum = new ThreeSum(new int[]{-2, 4, 8, 1, 2, 12, 5, 0, 9, -3});
         threeSum.Find(0);
     }
 }
