dbb5ddae1669d0923306dfe86867aca7da480bda
Oleksandr Bulakaiev <bulakaev@gmail.com>
2020-02-17 01:31:55 +0200

    RandomizedQueue added

--- Week2/Assignment/Permutation.java
+++ Week2/Assignment/Permutation.java
@@ -2,13 +2,12 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
-    private static RandomizedQueue<String> queue = new RandomizedQueue<>();
 
     public static void main(String[] args) {
-
         if (args.length < 1) {
             throw new IllegalArgumentException();
         }
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
         int k = Integer.parseInt(args[0]);
         int i = 0;
         while (!StdIn.isEmpty()) {
